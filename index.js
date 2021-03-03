189f4480ccc7de5ddefb3c722c3789cd3ce0d9b8
emsewell96 <emily.sewell.96@gmail.com>
2021-03-03 11:29:42 +0000

    javascript file to the FE

--- index.js
+++ index.js
@@ -1,17 +1,29 @@
-const express = require('express')
+const express = require("express");
 
-// Init app - an instance for express 
-const app = express()
+// Init app - an instance for express
+const app = express();
+
+const path = require("path");
 
 // get method route
 // respond with "hello world" when a GET request is made to the homepage
-app.get('/', function (req, res) {
-  res.send('<script>console.log("Hello World!")</script>')
-})
+app.get("/", function (req, res) {
+  //res.send('<script>console.log("Hello World!")</script>')
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
 
 // other route types - post, put, delete
-app.listen(3000, function(){
-  console.log('Server started on port 3000...')
-})
+app.listen(3000, function () {
+  console.log("Server started on port 3000...");
+});
 
 // dom - document object model the html that the browser is rendering (the tree of elements)
+// code you send to the client is being run on the client - not the server
+
+
+// homework:
+// get some form of data to the front end
+
+
+
+
